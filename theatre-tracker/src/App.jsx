8a0602b06c4e234c9b70f3db957b915cc12d3e4e
tescoboy <43451876+tescoboy@users.noreply.github.com>
2025-01-02 16:35:41 +0000

    Add files via upload

---
+++ theatre-tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Theatre Tracker</h1>
+      <p>Track your plays with ease!</p>
+    </div>
+  );
+}
+
+export default App;
